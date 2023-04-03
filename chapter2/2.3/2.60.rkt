1b1c6c87746b948d98e31325e4a1dfdca42ba879
Yan Zaycev <yzaytsev@voximplant.com>
2023-04-03 09:20:35 +0300

    Add 2.59 and 2.60

---
+++ chapter2/2.3/2.60.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set) (cons x set))
+
+(define (union-set set1 set2)
+    (if (null? set1) set2
+        (adjoin-set (car set1)
+                    (union-set (cdr set1) set2)
+        )
+    )
+)
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (adjoin-set (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
